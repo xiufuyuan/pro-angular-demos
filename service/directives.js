57ed79304effdf5735b37ac3493e915087fb9c94
xiufu.yuan <andlonger2016@gmail.com>
2016-07-06 15:53:52 +0800

    submit demo of ch19.

--- service/directives.js
+++ service/directives.js
@@ -31,5 +31,32 @@ angular.module('exampleDirectives', []).directive('triButton', function () {
         }
         element.text(result);
       })
-    }
+    };
+  })
+  .directive('interpolate', function ($interpolate) {
+    var fn = $interpolate('The total is: {{total | currency}} (including tax)');
+    return {
+      scope: {
+        amount: '=amount',
+        tax: '=tax'
+      },
+      link: function (scope, elemnt, attrs) {
+        scope.$watch('amount', function (newValue) {
+          var localData = {
+            total: +newValue + (+newValue * (+scope.tax / 100))
+          };
+          scope.total = localData.total;
+          elemnt.text(fn(scope));
+        })
+      }
+    };
+  })
+  .directive('compileDirective', function ($compile) {
+    return function (scope, element, attrs) {
+      var content = '<ul><li ng-repeat="city in data.cities">{{city}}</li></ul>';
+      var listElem = angular.element(content);
+      var compileFn = $compile(listElem);
+      compileFn(scope);
+      element.append(listElem);
+    };
   });
